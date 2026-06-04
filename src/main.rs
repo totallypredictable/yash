f0d9a0ccb9e73f3e4d8de533fe378c8e7bc09b8d
Ed <root@systemalert.org>
2026-06-04 15:49:47 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use std::env;
+use std::fs;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path::{Path, PathBuf};
+
+fn resolve_path(pathenv: &str, command: &str) -> Option<PathBuf> {
+    let rawpaths: Vec<&str> = pathenv.split(":").collect();
+    for path in rawpaths {
+        let path = Path::new(path).join(command);
+        if let Ok(metadata) = fs::metadata(&path) {
+            if metadata.is_file() && (metadata.permissions().mode() & 0o111) != 0 {
+                return Some(path);
+            }
+        }
+    }
+    return None;
+}
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    let pathenv = env::var("PATH").unwrap();
 
     loop {
         print!("$ ");
@@ -22,6 +39,12 @@ fn main() {
             "type" => {
                 if builtins.contains(&words[1]) {
                     println!("{} is a shell builtin", words[1]);
+                } else if resolve_path(&pathenv, words[1]) != None {
+                    println!(
+                        "{} is {:?}",
+                        words[1],
+                        resolve_path(&pathenv, words[1]).unwrap() // TODO unnecessary double call. Call the function one and match against its return?
+                    );
                 } else {
                     println!("{}: not found", words[1]);
                 }
