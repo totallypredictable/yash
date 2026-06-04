7f40f07ac03b4a3137a0de6df44d55e66c61a0d3
Ed <root@systemalert.org>
2026-06-04 14:06:45 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command: String = String::new();
+
+    io::stdin().read_line(&mut command).unwrap();
 }
