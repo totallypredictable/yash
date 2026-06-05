4551262be1d6e53706e342b02656a70d70242e24
Ed <root@systemalert.org>
2026-06-05 18:24:21 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
+use std::collections;
 use std::env;
 use std::fs;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::ops::ControlFlow;
-use std::os::unix::fs::PermissionsExt;
-use std::os::unix::process::CommandExt;
+use std::os::unix::{fs::PermissionsExt, process::CommandExt};
 use std::path::{Path, PathBuf};
 use std::process;
 
 const BUILTINS: &[&str] = &["exit", "echo", "type", "pwd", "cd"];
+
+fn env_ops<F, R>(action: F, cmd: &str, path: &Path) -> Option<R>
+where
+    F: FnOnce() -> io::Result<R>,
+{
+    match action() {
+        Ok(res) => Some(res),
+        Err(_) => {
+            eprintln!("{}: {}: No such file or directory", cmd, path.display());
+            None
+        }
+    }
+}
 
 fn resolve_path(pathenv: &str, command: &str) -> Option<PathBuf> {
     let rawpaths: Vec<&str> = pathenv.split(":").collect();
@@ -34,43 +47,83 @@ fn read_input() -> String {
     command
 }
 
-fn tokenize(command: &str) -> Vec<&str> {
-    let words: Vec<&str> = command.trim().split_whitespace().collect();
-    words
+enum TokenizerState {
+    Normal,
+    InSingleQuote,
+    InDoubleQuote,
 }
 
-struct ParsedCommand<'a> {
-    cmd: Command<'a>,
+fn tokenize(command: &str) -> Vec<String> {
+    let mut state = TokenizerState::Normal;
+    let mut args: Vec<String> = Vec::new();
+    let mut buffer = String::new();
+
+    for ch in command.chars() {
+        match (&state, ch) {
+            (TokenizerState::Normal, ' ') => {
+                if !buffer.is_empty() {
+                    args.push(std::mem::take(&mut buffer));
+                }
+            }
+            (TokenizerState::Normal, '"') => {
+                state = TokenizerState::InDoubleQuote;
+            }
+            (TokenizerState::Normal, '\'') => state = TokenizerState::InSingleQuote,
+            (TokenizerState::Normal, '~') => {
+                if let Ok(home) = env::var("HOME") {
+                    buffer.push_str(&home);
+                };
+            }
+            (TokenizerState::Normal, _) => {
+                buffer.push(ch);
+            }
+            (TokenizerState::InDoubleQuote, '"') => state = TokenizerState::Normal,
+            (TokenizerState::InDoubleQuote, _) => buffer.push(ch),
+            (TokenizerState::InSingleQuote, '\'') => state = TokenizerState::Normal,
+            (TokenizerState::InSingleQuote, _) => buffer.push(ch),
+        }
+    }
+
+    if !buffer.is_empty() {
+        args.push(buffer);
+    }
+    args
 }
 
-impl<'a> ParsedCommand<'a> {
-    fn new(words: &'a [&'a str]) -> Option<Self> {
-        if let [first_token, remaining_tokens @ ..] = words {
-            let command_type = match *first_token {
-                "exit" => Command::Exit,
-                "echo" => Command::Echo(remaining_tokens),
-                "type" => Command::Type(remaining_tokens),
-                "pwd" => Command::Pwd,
-                "cd" => Command::Cd(remaining_tokens),
-                _ => Command::External(first_token, remaining_tokens),
-            };
-            Some(ParsedCommand { cmd: command_type })
-        } else {
+enum Command {
+    Exit,
+    Echo(Vec<String>),
+    Type(Vec<String>),
+    Pwd,
+    Cd(Vec<String>),
+    External(String, Vec<String>),
+}
+
+struct ParsedCommand {
+    cmd: Command,
+}
+
+impl ParsedCommand {
+    fn new(mut words: Vec<String>) -> Option<Self> {
+        if words.is_empty() {
             return None;
         }
+        let mut words = collections::VecDeque::from(words);
+        let first_token: String = words.pop_front().unwrap();
+        let remaining_tokens = words.into_iter().collect();
+        let command_type = match first_token.as_str() {
+            "exit" => Command::Exit,
+            "echo" => Command::Echo(remaining_tokens),
+            "type" => Command::Type(remaining_tokens),
+            "pwd" => Command::Pwd,
+            "cd" => Command::Cd(remaining_tokens),
+            _ => Command::External(first_token, remaining_tokens),
+        };
+        Some(ParsedCommand { cmd: command_type })
     }
 }
 
-enum Command<'a> {
-    Exit,
-    Echo(&'a [&'a str]),
-    Type(&'a [&'a str]),
-    Pwd,
-    Cd(&'a [&'a str]),
-    External(&'a str, &'a [&'a str]),
-}
-
-fn dispatch_command(pathenv: &str, parsed_command: ParsedCommand<'_>) -> ControlFlow<()> {
+fn dispatch_command(pathenv: &str, parsed_command: ParsedCommand) -> ControlFlow<()> {
     match parsed_command.cmd {
         Command::Exit => ControlFlow::Break(()),
         Command::Echo(args) => {
@@ -79,9 +132,9 @@ fn dispatch_command(pathenv: &str, parsed_command: ParsedCommand<'_>) -> Control
         }
         Command::Type(cmds) => {
             for cmd in cmds {
-                if BUILTINS.contains(cmd) {
+                if BUILTINS.contains(&&cmd.as_str()) {
                     println!("{} is a shell builtin", cmd);
-                } else if let Some(path) = resolve_path(pathenv, cmd) {
+                } else if let Some(path) = resolve_path(pathenv, cmd.as_str()) {
                     println!("{} is {}", cmd, path.display(),);
                 } else {
                     println!("{}: not found", cmd);
@@ -96,15 +149,26 @@ fn dispatch_command(pathenv: &str, parsed_command: ParsedCommand<'_>) -> Control
             ControlFlow::Continue(())
         }
         Command::Cd(path) => {
-            let path = Path::new(path[0]);
-            if let Err(_) = env::set_current_dir(path) {
-                eprintln!("cd: {}: No such file or directory", path.display());
+            let target_path: Option<PathBuf> = match path.get(0) {
+                Some(p) => Some(PathBuf::from(p)),
+                None => match env::var("HOME") {
+                    Ok(h) => Some(PathBuf::from(h)),
+                    Err(_) => {
+                        eprintln!("cd: HOME not set");
+                        None
+                    }
+                },
+            };
+
+            if let Some(path) = target_path {
+                env_ops(|| env::set_current_dir(&path), "cd", &path);
             }
+
             ControlFlow::Continue(())
         }
         Command::External(bin, args) => {
-            if let Some(path) = resolve_path(pathenv, bin) {
-                run_program(&path, bin, args);
+            if let Some(path) = resolve_path(pathenv, bin.as_str()) {
+                run_program(&path, bin.as_str(), &args);
                 ControlFlow::Continue(())
             } else {
                 println!("{}: command not found", bin);
@@ -114,7 +178,7 @@ fn dispatch_command(pathenv: &str, parsed_command: ParsedCommand<'_>) -> Control
     }
 }
 
-fn run_program(path: &Path, bin: &str, args: &[&str]) {
+fn run_program(path: &Path, bin: &str, args: &[String]) {
     let mut cmd = process::Command::new(path);
     cmd.arg0(bin);
     cmd.args(args);
@@ -135,12 +199,29 @@ fn main() {
 
         let command = read_input();
 
-        let words = tokenize(&command);
+        let words = tokenize(&command.trim());
 
-        if let Some(parsed_command) = ParsedCommand::new(&words) {
+        if let Some(parsed_command) = ParsedCommand::new(words) {
             if let ControlFlow::Break(_) = dispatch_command(&pathenv, parsed_command) {
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tokenize() {
+        let command = "cd ~/Downloads/books";
+        assert_eq!(
+            tokenize(command),
+            Vec::from([
+                String::from("cd"),
+                String::from("/Users/nashjr/Downloads/books")
+            ])
+        );
     }
 }
