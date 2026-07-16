1d69185b8b2d0432ee7668613a62184af6283cba
Ed <root@systemalert.org>
2026-07-17 01:23:21 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -114,7 +114,9 @@ fn read_input(root: &TrieNode, complete_db: &HashMap<String, Vec<String>>) -> St
                         let outputs = stdout_result.split('\n');
 
                         for output in outputs {
-                            tmp.push(output.to_owned());
+                            if output.starts_with(&completion_prefix) {
+                                tmp.push(output.to_owned());
+                            }
                         }
                     } else {
                         let files = search_dir(&full_path);
