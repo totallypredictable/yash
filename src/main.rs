2db6c46bc630df9821fb28e5a4e4cbcaafae82b8
Ed <root@systemalert.org>
2026-07-17 01:54:23 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -591,8 +591,10 @@ fn run_completer_script(path: &Path, args: &Vec<String>) -> std::process::Output
     let args_list: Vec<String>;
     if args.len() == 2 {
         args_list = vec![args[0].clone(), args[1].clone(), String::from("")];
-    } else {
+    } else if args.len() == 3 {
         args_list = vec![args[0].clone(), args[2].clone(), args[1].clone()];
+    } else {
+        args_list = vec![String::from("")];
     }
     let cmd = process::Command::new(path)
         .args(args_list)
