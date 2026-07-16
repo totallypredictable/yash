5e7367cd8e46b2d0f1896dab838652354e8903b0
Ed <root@systemalert.org>
2026-07-17 01:32:05 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -110,7 +110,7 @@ fn read_input(root: &TrieNode, complete_db: &HashMap<String, Vec<String>>) -> St
 
                     if let Some(value) = complete_db.get(&args[0]) {
                         let output = run_completer_script(Path::new(&value[0]));
-                        eprintln!("STDOUT_RAW: {:#?}", output.stdout);
+                        eprintln!("STDOUT_RAW: {:?}", output.stdout);
                         let stdout_result = String::from_utf8(output.stdout).unwrap();
                         let outputs = stdout_result.split('\n');
 
